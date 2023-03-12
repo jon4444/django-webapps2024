ea51101c0518b444e1af3257e964552c35112d55
jon4444 <50190069+jon4444@users.noreply.github.com>
2023-03-12 18:40:47 +0000

    user authorisation

--- webapps2024/users/views.py
+++ webapps2024/users/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import RegisterForm
-from django.http import HttpResponse
+from . forms import UserRegisterForm
+# from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'users/home.html')
 
-def base(request):
-    return render(request, 'users/base.html')
+# def base(request):
+#     return render(request, 'users/base.html')
 
-def login(request):
-    return render(request, 'users/login.html')
+# def login(request):
+#     return render(request, 'users/login.html')
 
 def logout(request):
     # logout(request)
     return redirect('home')
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your account was created successfully')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+        
     return render(request, 'users/register.html', {'form':form})
     
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
--- webapps2024/webapps2024/settings.py
+++ webapps2024/webapps2024/settings.py
@@ -124,3 +124,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
