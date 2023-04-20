cfd23fdca95284936ed1876ffa88a5d99141be97
jon4444 <50190069+jon4444@users.noreply.github.com>
2023-04-20 20:52:50 +0100

    changed to postgresql db

--- webapps2024/webapps2024/settings.py
+++ webapps2024/webapps2024/settings.py
@@ -85,10 +85,29 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        
+    },
+    
+    'db.webapps': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db.webapps',
+        'USER': 'postgres',
+        'PASSWORD': 'arsenal4',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db.webapps',
+#         'USER': 'postgres',
+#         'PASSWORD': 'arsenal4',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
